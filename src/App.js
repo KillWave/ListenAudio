7b15d06e0c15bcdfa50105ca2697af57c57ebddb
KillWave <1353207933@qq.com>
2020-03-19 14:22:37 +0800

    deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/serviceWorker.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState,useEffect,createContext,useContext} from 'react';
 import './App.css';
 
+let CountContext = createContext();
+function CountChild(){
+  let count = useContext(CountContext)
+return (<h2 >{count}</h2>)
+}
 function App() {
+  useEffect(()=>{
+  },[]);
+
+
+  let [count ,setCount] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={()=>{setCount(count+1)}}>click me</button>
+      <CountContext.Provider value={count}></CountContext.Provider>
     </div>
   );
 }
+
 
 export default App;
